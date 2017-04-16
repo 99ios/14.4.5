bcb605e602398f096c281f40a3f456df28fe1bda
test <422538722@qq.com>
2017-04-16 16:28:46 +0800

    14.4.5
     CAKeyframeAnimation关键帧动画

---
+++ 14.4.5 CAKeyframeAnimation关键帧动画/14.4.5 CAKeyframeAnimation关键帧动画/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  14.4.5 CAKeyframeAnimation关键帧动画
+//
+//  Created by 李维佳 on 2017/4/7.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) CALayer *myLayer;
+@end
+
+@implementation ViewController
+
+- (CALayer *)myLayer{
+    if (_myLayer == nil) {
+        _myLayer = [CALayer layer];
+        _myLayer.frame = CGRectMake(140, 100, 100, 100);
+        _myLayer.backgroundColor = [UIColor yellowColor].CGColor;
+        _myLayer.borderColor = [UIColor redColor].CGColor;
+        _myLayer.borderWidth = 4.0;
+        _myLayer.cornerRadius = 2.0;
+    }
+    return _myLayer;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view.layer addSublayer:self.myLayer];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //1. 实例化CAKeyframeAnimation对象
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
+    //2. 设置动画属性
+    animation.keyPath = @"transform.translation.x";
+    animation.values = @[@0,@100,@0];
+    animation.duration = 2.0;
+    animation.repeatCount = MAXFLOAT;
+    //3. 添加动画对象到一个CALayer类的对象上，播放动画
+    [self.myLayer addAnimation:animation forKey:nil];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
